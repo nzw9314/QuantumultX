a86af9f4c1f63ea346d1a3d8a75dfe357eade5c3
刘小康 <lxk@mchuan.com>
2020-06-22 13:52:11 +0800

    京东种豆得豆和宠汪汪脚本兼容Loon和surge。

--- Task/jd_joy.js
+++ Task/jd_joy.js
@@ -1,7 +1,139 @@
 //jd宠汪汪 搬的https://github.com/uniqueque/QuantumultX/blob/4c1572d93d4d4f883f483f907120a75d925a693e/Script/jd_joy.js
-
-//只能quanx用，request里面的请求跟获取cookie的地方改改，别的app应该也能用
-
+/******************** 转换器 ********************/
+let isQuantumultX = $task != undefined;
+let isSurge = $httpClient != undefined;
+let isLoon = isSurge && typeof $loon != undefined;
+var $task = isQuantumultX ? $task : {};
+var $httpClient = isSurge ? $httpClient : {};
+var $prefs = isQuantumultX ? $prefs : {};
+var $persistentStore = isSurge ? $persistentStore : {};
+var $notify = isQuantumultX ? $notify : {};
+var $notification = isSurge ? $notification : {};
+if (isQuantumultX) {
+  var errorInfo = {
+    error: "",
+  };
+  $httpClient = {
+    get: (url, cb) => {
+      var urlObj;
+      if (typeof url == "string") {
+        urlObj = {
+          url: url,
+        }
+      } else {
+        urlObj = url
+      }
+      $task.fetch(urlObj).then((response) => {
+        cb(undefined, response, response.body)
+      }, (reason) => {
+        errorInfo.error = reason.error;
+        cb(errorInfo, response, "")
+      })
+    }, post: (url, cb) => {
+      var urlObj;
+      if (typeof url == "string") {
+        urlObj = {
+          url: url,
+        }
+      } else {
+        urlObj = url
+      }
+      url.method = "POST"
+      $task.fetch(urlObj).then((response) => {
+        cb(undefined, response, response.body)
+      }, (reason) => {
+        errorInfo.error = reason.error;
+        cb(errorInfo, response, "")
+      })
+    },
+  }
+}
+if (isSurge) {
+  $task = {
+    fetch: (url) => {
+      return new Promise((resolve, reject) => {
+        if (url.method == "POST") {
+          $httpClient.post(url, (error, response, data) => {
+            if (response) {
+              response.body = data;
+              resolve(response, {
+                error: error,
+              })
+            } else {
+              resolve(null, {
+                error: error,
+              })
+            }
+          })
+        } else {
+          $httpClient.get(url, (error, response, data) => {
+            if (response) {
+              response.body = data;
+              resolve(response, {
+                error: error,
+              })
+            } else {
+              resolve(null, {
+                error: error,
+              })
+            }
+          })
+        }
+      })
+    },
+  }
+}
+if (isQuantumultX) {
+  $persistentStore = {
+    read: (key) => {
+      return $prefs.valueForKey(key)
+    }, write: (val, key) => {
+      return $prefs.setValueForKey(val, key)
+    },
+  }
+}
+if (isSurge) {
+  $prefs = {
+    valueForKey: (key) => {
+      return $persistentStore.read(key)
+    }, setValueForKey: (val, key) => {
+      return $persistentStore.write(val, key)
+    },
+  }
+}
+if (isQuantumultX) {
+  $notify = ((notify) => {
+    return function (title, subTitle, detail, url = undefined) {
+      detail = url === undefined ? detail : `${detail}\n点击链接跳转: ${url}`;
+      notify(title, subTitle, detail)
+    }
+  })($notify);
+  $notification = {
+    post: (title, subTitle, detail, url = undefined) => {
+      detail = url === undefined ? detail : `${detail}\n点击链接跳转: ${url}`;
+      $notify(title, subTitle, detail)
+    },
+  }
+}
+if (isSurge && !isLoon) {
+  $notification.post = ((notify) => {
+    return function (title, subTitle, detail, url = undefined) {
+      detail = url === undefined ? detail : `${detail}\n点击链接跳转: ${url}`;
+      notify.call($notification, title, subTitle, detail)
+    }
+  })($notification.post);
+  $notify = (title, subTitle, detail, url = undefined) => {
+    detail = url === undefined ? detail : `${detail}\n点击链接跳转: ${url}`;
+    $notification.post(title, subTitle, detail)
+  }
+}
+if (isLoon) {
+  $notify = (title, subTitle, detail, url = undefined) => {
+    $notification.post(title, subTitle, detail, url)
+  }
+}
+/******************** 转换器 ********************/
+// 兼容Loon,surge等app，转化器代码来自：https://github.com/Peng-YM/ScriptConverter
 //直接用NobyDa的jd cookie
 const cookie = $prefs.valueForKey('CookieJD')
 const name = '京东宠汪汪'
