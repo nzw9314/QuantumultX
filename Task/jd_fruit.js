d3653572bd4d36a36a9488e80e9dc63eca76a16a
YuanHuiZeng <790094613@qq.com>
2020-07-20 10:53:38 +0800

    更新京东农场

--- Task/jd_fruit.js
+++ Task/jd_fruit.js
@@ -28,9 +28,6 @@ let jdNotify = $.getdata('jdFruitNotify');
 let shareCodes = [ // 这个列表填入你要助力的好友的shareCode
   'a6f686a9f6aa4c80977370b03681c553',
   'f92cb56c6a1349f5a35f0372aa041ea0',
-  'a9360baeceb04c9baaaa109f5d428d3c',
-  '61ff5c624949454aa88561f2cd721bf6',
-  '40dbf12bb7ea4b8eb772741afe2125da'
 ]
 // 添加box功能
 // 【用box订阅的好处】
