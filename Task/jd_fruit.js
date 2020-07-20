7abf4f247dfc495e2cb7851b82ecbcc05b335259
YuanHuiZeng <790094613@qq.com>
2020-07-20 11:45:50 +0800

    测试远程仓库

--- Task/jd_fruit.js
+++ Task/jd_fruit.js
@@ -28,6 +28,7 @@ let jdNotify = $.getdata('jdFruitNotify');
 let shareCodes = [ // 这个列表填入你要助力的好友的shareCode
   'a6f686a9f6aa4c80977370b03681c553',
   'f92cb56c6a1349f5a35f0372aa041ea0',
+    '    '
 ]
 // 添加box功能
 // 【用box订阅的好处】
